bcee3710a375486ca68174b682ae420dcb69f01e
Juan Marin <jmo129@gmail.com>
2025-04-02 23:39:14 +0200

    Añadido React Big Calendar. Funciona

--- src/pages/Calendario.jsx
+++ src/pages/Calendario.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import MatchCard from '../components/MatchCard'
 import service from '../services/config.services';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import moment from 'moment';
+import 'moment-timezone'
 
 
 function Calendario() {
   
   const [listMatches, setListMatches] = useState(null);
 
-  
   useEffect(() => {
     getMatches();
   },[])
@@ -27,12 +30,58 @@ function Calendario() {
     return <h3>Espere por favor. Conectando con la data...</h3>
   }
 
-
+  // Aquí creamos las características del Calendar
+  moment.updateLocale('es', {
+    months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    weekdaysShort : ["Dom", "Lun", "Mar", "Mier", "Jue", "Vier", "Sab"],
+    week: {
+      dow:1
+    }
+});
+  moment.tz.setDefault('Europe/Madrid')
+  const localizer = momentLocalizer(moment)
+  console.log("Esta es la lista de partidos para calendario")
   console.log(listMatches);
-  return (
-    <div>
-      <h1>Calendario Partidos</h1>
 
+
+
+  const events = []
+
+  listMatches.map((eachMatch) => {
+    let dataEvent = {
+      start: moment(eachMatch.fecha),
+      end: moment(eachMatch.fecha),
+      title: eachMatch.equipoRival
+    }
+    events.push(dataEvent)
+  })
+ 
+ 
+  //events.push(partido)
+
+  return (
+    <div style={{height:"95vh", witdh:"95vw"}}>
+      <Calendar
+      localizer={localizer}
+      events={events}
+      views={["month"]}
+      culture='es'
+      messages={{
+        next: "Siguiente",
+        previous: "Anterior",
+        today: "Hoy",
+        month: "Mes",
+        week: "Semana"
+      }}
+      />
+    </div>
+  )
+}
+
+export default Calendario
+
+
+/* 
       <div>
         {listMatches.map((eachMatch, index) => {
           return (
@@ -42,9 +91,4 @@ function Calendario() {
             />
           )
         })}
-      </div>
-    </div>
-  )
-}
-
-export default Calendario
+      </div> */
