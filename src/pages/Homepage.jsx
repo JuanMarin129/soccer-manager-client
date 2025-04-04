5f19312a57d43ebc56f3e2438fd56bce2e6763ca
Juan Marin <jmo129@gmail.com>
2025-04-04 12:01:21 +0200

    Background Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import background from "../assets/Background_Homepage_01.jpg"
 
 function Homepage() {
   return (
     <div>
         
         <h1>Homepage</h1>
+        <img width={"1280px"} height={"720px"} src={background}    />
       
 
     </div>
